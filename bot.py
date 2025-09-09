5ed1f5b9a7f767dc3859dfc7f45343d5e6df6082
YskvAnn <yaskovaann2002@mail.ru>
2025-09-09 12:34:52 +0300

    optimisation all

--- bot.py
+++ bot.py
@@ -1,18 +1,25 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, WebAppInfo
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, Application
+from telegram import Bot
+import asyncio
 
+TOKEN = "8293900584:AAGnknhlDJzhXo8ZqjwPlZS9irtPBJUm1ZI"
+
+async def clear_updates():
+    bot = Bot(TOKEN)
+    updates = await bot.get_updates(offset=-1)
+    if updates:
+        print(f"Пропущено {len(updates)} старых апдейтов")
+    else:
+        print("Старых апдейтов нет")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # Создаем кнопку Web App с объектом WebAppInfo
     keyboard = [
         [InlineKeyboardButton("Открыть Web App", web_app=WebAppInfo(url="https://yskvann.github.io/SlowFlow/"))]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-
-    # Отправляем сообщение с кнопкой
     await update.message.reply_text("Нажми кнопку:", reply_markup=reply_markup)
 
-
-app = ApplicationBuilder().token("8253449029:AAGVWHt8F1fk82jooUMmkKqU3LCh-AAmt3Y").build()
-app.add_handler(CommandHandler("start", start))
-app.run_polling()
+async def main():
+    await clear_updates()
+    app: Application = ApplicationBuilder().t
